5944b4ed780a578bd1429ab25ee7a8ecc68cca7a
bhanudadhich <bhanudadhich107@gmail.com>
2026-06-03 13:23:59 +0530

    add all

---
+++ MergeIntervals.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MergeIntervals {
+
+    public static int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] current = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
+            }
+        }
+
+        result.add(current);
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+
+        int[][] ans = merge(intervals);
+
+        for (int[] x : ans)
+            System.out.println(x[0] + " " + x[1]);
+    }
+}
